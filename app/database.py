151e75d07f5bd7fe4b8b65b7c87b0de530cd2526
djemmal <islamdjemmal7@gmail.com>
2024-05-24 01:06:57 +0200

    add the database and collection

--- app/database.py
+++ app/database.py
@@ -6,11 +6,17 @@ import dotenv
 
 dotenv.load_dotenv()
 url = os.getenv("DB_URL")
+
 #Create a new client and connect to the server
 client = MongoClient(url, server_api=ServerApi('1'))
 #Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
-#   print("Pinged your deployment. You successfully connected to MongoDB!")
+    # print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+
+
+#Create database and collection
+db = client.MarketPlace
+collection = db["products"]
