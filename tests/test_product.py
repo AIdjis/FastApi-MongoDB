f34203345fe72c59b6093a85590ddacdfd2d3f31
djemmal <islamdjemmal7@gmail.com>
2024-06-10 00:51:37 +0200

    📝 test creating a new product.

--- tests/test_product.py
+++ tests/test_product.py
@@ -8,4 +8,13 @@ def test_read_item():
     response = client.get("/Products")
     assert response.status_code == 200
 
-
+# test for creating a new product
+def test_create_product():
+    response = client.post("/Products",json={
+        "name":"test",
+        "description":"test",
+        "price":100,
+        "quantity":10,
+        "is_available":True
+    })
+    assert response.status_code == 201
