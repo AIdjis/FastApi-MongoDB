5e1a193c222c1e2136376e7130b359ba937f88f2
djemmal <islamdjemmal7@gmail.com>
2024-07-25 20:42:32 +0100

    ✨ Add logout user endpoint

--- app/profile.py
+++ app/profile.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Depends,HTTPException,status
+from fastapi import APIRouter,Depends,HTTPException,status,Response
 from app.database import client
 from bson import ObjectId
 from datetime import datetime
@@ -16,13 +16,21 @@ def deserialize_data(user):
             "email":user["email"],
             "created_at":user["created_at"]}
 
-
+# retrieve user profile data
 @profile_router.get("/",status_code=status.HTTP_200_OK,response_model=UserProfile)
 async def get_profile(user: dict =Depends(jwt_required)):
     profile=client.MarketPlace.users.find_one({"_id":ObjectId(user["id"])})
     if profile==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="profile not found")
     return deserialize_data(profile)
+
+#logout the user
+@profile_router.get('/logout', status_code=status.HTTP_200_OK)
+def logout(response: Response, Authorize: dict = Depends(jwt_required)):
+    response.delete_cookie(key='access_token', domain=None, httponly=True, samesite="lax")
+    response.delete_cookie('refresh_token', domain=None, httponly=True, samesite="lax")
+    response.set_cookie('logged_in', '', -1)
+    return {'detail': 'success'}
 
 
 
